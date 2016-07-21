da9fb2ed54c00bd704070f7e395b45dfcbb20ead
christianbltr <christian.buelter@inmedias.de>
2016-07-21 16:31:58 +0200

    [TASK] change label and position of doktype configuration field in indexer config TCA

--- Configuration/TCA/tx_kesearch_indexerconfig.php
+++ Configuration/TCA/tx_kesearch_indexerconfig.php
@@ -17,7 +17,7 @@ $configurationArray = array(
         'requestUpdate' => 'type'
     ),
     'interface' => array(
-        'showRecordFieldList' => 'hidden,title,storagepid,startingpoints_recursive,single_pages,sysfolder,type,index_content_with_restrictions,index_passed_events,,index_news_category_mode,index_news_category_selection,directories,fileext,filteroption,index_page_doctypes'
+        'showRecordFieldList' => 'hidden,title,storagepid,startingpoints_recursive,single_pages,sysfolder,type,index_content_with_restrictions,index_passed_events,index_news_category_mode,index_news_category_selection,directories,fileext,filteroption,index_page_doctypes'
     ),
     'columns' => array(
         'hidden' => array(
@@ -449,7 +449,7 @@ $configurationArray = array(
         )
     ),
     'types' => array(
-        '0' => array('showitem' => 'hidden;;1;;1-1-1, title;;;;2-2-2, storagepid,targetpid;;;;3-3-3,type,startingpoints_recursive,single_pages,sysfolder,index_page_doctypes,index_content_with_restrictions,index_passed_events,index_news_archived,index_news_category_mode,index_news_category_selection,index_extnews_category_selection,index_news_useHRDatesSingle,index_news_useHRDatesSingleWithoutDay,index_use_page_tags,fal_storage,directories,fileext,contenttypes,commenttypes,filteroption,tvpath,index_use_page_tags_for_files,cal_expired_events')
+        '0' => array('showitem' => 'hidden;;1;;1-1-1, title;;;;2-2-2, storagepid,targetpid;;;;3-3-3,type,startingpoints_recursive,single_pages,sysfolder,index_content_with_restrictions,index_passed_events,index_news_archived,index_news_category_mode,index_news_category_selection,index_extnews_category_selection,index_news_useHRDatesSingle,index_news_useHRDatesSingleWithoutDay,index_use_page_tags,fal_storage,directories,fileext,index_page_doctypes,contenttypes,commenttypes,filteroption,tvpath,index_use_page_tags_for_files,cal_expired_events')
     ),
     'palettes' => array(
         '1' => array('showitem' => '')
