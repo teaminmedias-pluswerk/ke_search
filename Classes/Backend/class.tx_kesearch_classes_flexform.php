0d9495de9f01792575c38f29c7900a1d11110fda
christianbltr <christian.buelter@inmedias.de>
2016-07-21 19:57:00 +0200

    restructure order tab in flexform configuration, add ke_search_premium fields to "not allowed fields" for sorting

--- Classes/Backend/class.tx_kesearch_classes_flexform.php
+++ Classes/Backend/class.tx_kesearch_classes_flexform.php
@@ -4,6 +4,7 @@ class tx_kesearch_classes_flexform {
 	 * @var language
 	 */
 	var $lang;
+	var $notAllowedFields = 'uid,pid,tstamp,crdate,cruser_id,starttime,endtime,fe_group,targetpid,content,params,type,tags,abstract,language,orig_uid,orig_pid,hash,lat,lon,externalurl,lastremotetransfer';
 
 	public function init() {
 		$this->lang = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Lang\\LanguageService');
@@ -15,12 +16,11 @@ class tx_kesearch_classes_flexform {
 
 		// get orderings
 		$fieldLabel = $this->lang->sL('LLL:EXT:ke_search/locallang_db.php:tx_kesearch_index.relevance');
-		$notAllowedFields = 'uid,pid,tstamp,crdate,cruser_id,starttime,endtime,fe_group,targetpid,content,params,type,tags,abstract,language,orig_uid,orig_pid,hash';
 		$config['items'][] = array($fieldLabel, 'score');
 		$res = $GLOBALS['TYPO3_DB']->sql_query('SHOW COLUMNS FROM tx_kesearch_index');
 		while($col = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res)) {
 
-			$isInList = TYPO3\CMS\Core\Utility\GeneralUtility::inList($notAllowedFields, $col['Field']);
+			$isInList = TYPO3\CMS\Core\Utility\GeneralUtility::inList($this->notAllowedFields, $col['Field']);
 			if(!$isInList) {
 				$file = $GLOBALS['TCA']['tx_kesearch_index']['columns'][$col['Field']]['label'];
 				$fieldLabel = $this->lang->sL($file);
@@ -35,7 +35,6 @@ class tx_kesearch_classes_flexform {
 
 		// get orderings
 		$fieldLabel = $this->lang->sL('LLL:EXT:ke_search/locallang_db.php:tx_kesearch_index.relevance');
-		$notAllowedFields = 'uid,pid,tstamp,crdate,cruser_id,starttime,endtime,fe_group,targetpid,content,params,type,tags,abstract,language,orig_uid,orig_pid,hash';
 		if(!$config['config']['relevanceNotAllowed']) {
 			$config['items'][] = array($fieldLabel . ' UP', 'score asc');
 			$config['items'][] = array($fieldLabel . ' DOWN', 'score desc');
@@ -43,7 +42,7 @@ class tx_kesearch_classes_flexform {
 		$res = $GLOBALS['TYPO3_DB']->sql_query('SHOW COLUMNS FROM tx_kesearch_index');
 		while($col = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res)) {
 
-			$isInList = TYPO3\CMS\Core\Utility\GeneralUtility::inList($notAllowedFields, $col['Field']);
+			$isInList = TYPO3\CMS\Core\Utility\GeneralUtility::inList($this->notAllowedFields, $col['Field']);
 			if(!$isInList) {
 				$file = $GLOBALS['TCA']['tx_kesearch_index']['columns'][$col['Field']]['label'];
 				$fieldLabel = $this->lang->sL($file);
