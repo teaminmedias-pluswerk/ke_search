c7e5db1069cf219d2f27a15f799fef5ab959f750
pixelbrackets <mail@pixelbrackets.de>
2017-08-15 14:34:16 +0200

    [BUGFIX] Use correct ellipsis punctuation mark
    The ellipsis is a series of dots to indicates an
    intentional omission. The ellipsis is a single character
    (…) instead of three full stops (...).

--- Classes/lib/class.tx_kesearch_lib_searchresult.php
+++ Classes/lib/class.tx_kesearch_lib_searchresult.php
@@ -271,7 +271,7 @@ class tx_kesearch_lib_searchresult
 
                 // crop some words behind searchword
                 $partWithSearchWord = substr($content, $startPos);
-                $temp = $this->cObj->crop($partWithSearchWord, $charsForEachSearchWord . '|...|1');
+                $temp = $this->cObj->crop($partWithSearchWord, $charsForEachSearchWord . '|…|1');
 
                 // crop some words before searchword
                 // after last cropping our text is too short now. So we have to find a new cutting position
@@ -286,7 +286,7 @@ class tx_kesearch_lib_searchresult
             } elseif ($isSearchWordAtTheBeginning === true) {
                 $teaser = implode(' ', $teaserArray);
             } else {
-                $teaser = '...' . implode(' ', $teaserArray);
+                $teaser = '…' . implode(' ', $teaserArray);
             }
 
             // highlight hits?
@@ -295,7 +295,7 @@ class tx_kesearch_lib_searchresult
             }
             return $teaser;
         } else {
-            return $this->cObj->crop($content, $this->conf['resultChars'] . '|...|1');
+            return $this->cObj->crop($content, $this->conf['resultChars'] . '|…|1');
         }
     }
 }
