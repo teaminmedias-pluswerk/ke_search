7057ca68736b3ee1117cc600b5db8a7cbd51b49b
Christian Bülter <christian.buelter@pluswerk.ag>
2018-09-04 13:58:45 +0200

    [TASK] add dependency to typo3db-legacy and TYPO3 v9 Core

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,7 +23,8 @@ $EM_CONF[$_EXTKEY] = array(
     'CGLcompliance_note' => '',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '7.6.0-8.7.99',
+            'typo3' => '9.3.0-9.3.99',
+            'typo3db_legacy' => '1.0.0-1.0.99'
         ),
         'conflicts' => array(),
         'suggests' => array(),
