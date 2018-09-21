fd4b6ca45f19ca2daaf656dad9a993eac56869ec
Christian Bülter <christian.buelter@pluswerk.ag>
2018-09-21 14:44:51 +0200

    [TASK] set version constraints in ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,7 +23,7 @@ $EM_CONF[$_EXTKEY] = array(
     'CGLcompliance_note' => '',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '9.3.0-9.3.99',
+            'typo3' => '9.3.0-9.4.99',
             'typo3db_legacy' => '1.0.0-1.0.99'
         ),
         'conflicts' => array(),
