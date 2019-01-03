fdb613549b6f677a0edbf1aeaf83befc66a0bf9b
Andreas Kiefer <andreas.kiefer@pluswerk.ag>
2019-01-03 11:50:27 +0100

    [BUGFIX] remove obsolete classmaps
    see [#254]
    * obsolete class maps lead to errors when installing the ext in no-composer-mode

--- ext_emconf.php
+++ ext_emconf.php
@@ -31,6 +31,6 @@ $EM_CONF[$_EXTKEY] = array(
     'suggests' => array(),
     'autoload' => array(
         'psr-4' => array('TeaminmediasPluswerk\\KeSearch\\' => 'Classes'),
-        'classmap' => array('Classes', 'pi1', 'pi2'),
+        'classmap' => array('Classes'),
     ),
 );
