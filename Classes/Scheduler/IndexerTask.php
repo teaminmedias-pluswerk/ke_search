4350e26a54debf7d95c02ccb96dd732300ce49b7
Andreas Kiefer <andreas.kiefer@pluswerk.ag>
2019-01-08 19:50:17 +0100

    [BUGFIX] add missing class alias for ExtensionConfiguration

--- Classes/Scheduler/IndexerTask.php
+++ Classes/Scheduler/IndexerTask.php
@@ -1,6 +1,7 @@
 <?php
 namespace TeaminmediasPluswerk\KeSearch\Scheduler;
 
+use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /***************************************************************
