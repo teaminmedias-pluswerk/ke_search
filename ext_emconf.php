1afc4a812e4246aa8063170c7d2ad3a18bc3e427
Christian Bülter <buelter@conquer.de>
2020-04-24 12:52:17 +0200

    [TASK] Raise maximum TYPO3 version to 10.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,7 +23,7 @@ $EM_CONF[$_EXTKEY] = array(
     'CGLcompliance_note' => '',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '9.5.0-10.3.99'
+            'typo3' => '9.5.0-10.4.99'
         ),
         'conflicts' => array(),
         'suggests' => array(),
