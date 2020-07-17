694a7fa270f77c88c2f44caa445dcb552465cecf
Christian Bülter <christian.buelter@web.de>
2020-07-17 13:18:04 +0200

    [TASK] Cleanup ext_localconf.php and ext_tables.php, closes #361

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,60 +3,63 @@ if (!defined('TYPO3_MODE')) {
     die ('Access denied.');
 }
 
-$_EXTKEY = 'ke_search';
+(function () {
 
-// add Searchbox Plugin, override class name with namespace
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPItoST43($_EXTKEY, '', '_pi1');
-$overrideSetup = 'plugin.tx_kesearch_pi1.userFunc = TeaminmediasPluswerk\KeSearch\Plugins\SearchboxPlugin->main';
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript('tx_kesearch', 'setup', $overrideSetup);
+    // add Searchbox Plugin, override class name with namespace
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPItoST43('ke_search', '', '_pi1');
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
+        'tx_kesearch',
+        'setup',
+        'plugin.tx_kesearch_pi1.userFunc = TeaminmediasPluswerk\KeSearch\Plugins\SearchboxPlugin->main'
+    );
 
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPItoST43($_EXTKEY, '', '_pi2');
-$overrideSetup = 'plugin.tx_kesearch_pi2.userFunc = TeaminmediasPluswerk\KeSearch\Plugins\ResultlistPlugin->main';
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript('tx_kesearch', 'setup', $overrideSetup);
+    // add Resultlist Plugin, override class name with namespace
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPItoST43('ke_search', '', '_pi2');
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
+        'tx_kesearch',
+        'setup',
+        'plugin.tx_kesearch_pi2.userFunc = TeaminmediasPluswerk\KeSearch\Plugins\ResultlistPlugin->main'
+    );
 
-// add page TSconfig (Content element wizard icons, hide index table)
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:ke_search/Configuration/TSconfig/Page/pageTSconfig.txt">'
-);
+    // add page TSconfig (Content element wizard icons, hide index table)
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+        '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:ke_search/Configuration/TSconfig/Page/pageTSconfig.txt">'
+    );
 
-// use hooks for generation of sortdate values
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['registerAdditionalFields'][] =
-    TeaminmediasPluswerk\KeSearch\Hooks\AdditionalFields::class;
+    // use hooks for generation of sortdate values
+    $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['registerAdditionalFields'][] =
+        \TeaminmediasPluswerk\KeSearch\Hooks\AdditionalFields::class;
 
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['modifyPagesIndexEntry'][] =
-    TeaminmediasPluswerk\KeSearch\Hooks\AdditionalFields::class;
+    $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['modifyPagesIndexEntry'][] =
+        \TeaminmediasPluswerk\KeSearch\Hooks\AdditionalFields::class;
 
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['modifyContentIndexEntry'][] =
-    TeaminmediasPluswerk\KeSearch\Hooks\AdditionalFields::class;
+    $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['modifyContentIndexEntry'][] =
+        \TeaminmediasPluswerk\KeSearch\Hooks\AdditionalFields::class;
 
-// add scheduler task
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][\TeaminmediasPluswerk\KeSearch\Scheduler\IndexerTask::class] = array(
-    'extension' => $_EXTKEY,
-    'title' => 'Indexing process for ke_search',
-    'description' => 'This task updates the ke_search index'
-);
+    // Custom validators for TCA (eval)
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tce']['formevals']
+    ['TeaminmediasPluswerk\\KeSearch\\UserFunction\\CustomFieldValidation\\FilterOptionTagValidator'] =
+        'EXT:ke_search/Classes/UserFunction/CustomFieldValidation/FilterOptionTagValidator.php';
 
-// Custom validators for TCA (eval)
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tce']['formevals']
-['TeaminmediasPluswerk\\KeSearch\\UserFunction\\CustomFieldValidation\\FilterOptionTagValidator'] =
-    'EXT:ke_search/Classes/UserFunction/CustomFieldValidation/FilterOptionTagValidator.php';
+    // logging
+    $extConf = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+        \TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class
+    )->get('ke_search');
+    $loglevel = !empty($extConf['loglevel']) ? $extConf['loglevel'] : 'ERROR';
 
-// logging
-$extConf = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class)->get('ke_search');
-$loglevel = !empty($extConf['loglevel']) ? $extConf['loglevel'] : 'ERROR';
+    if (\TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(TYPO3_branch) <
+        \TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger('10.0')
+    ) {
+        $loglevel = \TYPO3\CMS\Core\Log\LogLevel::normalizeLevel($loglevel);
+    } else {
+        $loglevel = strtolower($loglevel);
+    }
 
-if (\TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(TYPO3_branch) <
-    \TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger('10.0')
-) {
-    $loglevel = \TYPO3\CMS\Core\Log\LogLevel::normalizeLevel($loglevel);
-} else {
-    $loglevel = strtolower($loglevel);
-}
-
-$GLOBALS['TYPO3_CONF_VARS']['LOG']['TeaminmediasPluswerk']['KeSearch']['Indexer']['writerConfiguration'] = [
-     $loglevel => [
-        'TYPO3\\CMS\\Core\\Log\\Writer\\FileWriter' => [
-            'logFileInfix' => 'kesearch'
+    $GLOBALS['TYPO3_CONF_VARS']['LOG']['TeaminmediasPluswerk']['KeSearch']['Indexer']['writerConfiguration'] = [
+        $loglevel => [
+            'TYPO3\\CMS\\Core\\Log\\Writer\\FileWriter' => [
+                'logFileInfix' => 'kesearch'
+            ]
         ]
-    ]
-];
+    ];
+})();
--- ext_tables.php
+++ ext_tables.php
@@ -4,22 +4,19 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-$_EXTKEY = 'ke_search';
+(function () {
+    // add help file
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
+        'tx_kesearch_filters',
+        'EXT:ke_search/locallang_csh.xml'
+    );
 
-// add help file
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
-    'tx_kesearch_filters',
-    'EXT:ke_search/locallang_csh.xml'
-);
-
-// add module
-if (TYPO3_MODE == 'BE') {
-
+    // add module
     if (\TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(TYPO3_branch) >=
         \TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger('10.0')
     ) {
         $extensionName = 'ke_search';
-        $controller = TeaminmediasPluswerk\KeSearch\Controller\BackendModuleController::class;
+        $controller = \TeaminmediasPluswerk\KeSearch\Controller\BackendModuleController::class;
     } else {
         $extensionName = 'TeaminmediasPluswerk.ke_search';
         $controller = 'BackendModule';
@@ -27,26 +24,34 @@ if (TYPO3_MODE == 'BE') {
 
     \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
         $extensionName,
-        'web',          // Main area
-        'backend_module',         // Name of the module
-        '',             // Position of the module
-        array(          // Allowed controller action combinations
+        'web',
+        'backend_module',
+        '',
+        array(
             $controller =>
                 'startIndexing,indexedContent,indexTableInformation,'
                 . 'searchwordStatistics,clearSearchIndex,lastIndexingReport,alert',
         ),
-        array(          // Additional configuration
+        array(
             'access' => 'user,group',
             'icon' => 'EXT:ke_search/Resources/Public/Icons/moduleicon.svg',
             'labels' => 'LLL:EXT:ke_search/Resources/Private/Language/locallang_mod.xml',
         )
     );
-}
 
-/** @var \TYPO3\CMS\Core\Imaging\IconRegistry $iconRegistry */
-$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-$iconRegistry->registerIcon(
-    'ext-kesearch-wizard-icon',
-    'TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider',
-    ['source' => 'EXT:ke_search/Resources/Public/Icons/moduleicon.svg']
-);
+    // add scheduler task
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][\TeaminmediasPluswerk\KeSearch\Scheduler\IndexerTask::class]
+        = array(
+        'extension' => 'ke_search',
+        'title' => 'Indexing process for ke_search',
+        'description' => 'This task updates the ke_search index'
+    );
+
+    /** @var \TYPO3\CMS\Core\Imaging\IconRegistry $iconRegistry */
+    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+    $iconRegistry->registerIcon(
+        'ext-kesearch-wizard-icon',
+        'TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider',
+        ['source' => 'EXT:ke_search/Resources/Public/Icons/moduleicon.svg']
+    );
+})();
